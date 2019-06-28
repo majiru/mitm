0c4c624551210ea81dbe29a5d31d564a40c42dc8
Jacob Moody <jsmoody@iastate.edu>
2019-06-28 00:17:07 -0500

    Use cert_test.go certs instead of generating another pair

--- try.go
+++ try.go
@@ -6,11 +6,9 @@ import (
 	"bytes"
 	"crypto/tls"
 	"crypto/x509"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"path"
 	"strings"
 
 	"github.com/majiru/mitm"
@@ -19,9 +17,8 @@ import (
 var (
 	hostname, _ = os.Hostname()
 
-	dir      = path.Join(os.Getenv("HOME"), ".mitm")
-	keyFile  = path.Join(dir, "ca-key.pem")
-	certFile = path.Join(dir, "ca-cert.pem")
+	keyFile  = "./key.pem"
+	certFile = "./cert.pem"
 )
 
 func main() {
@@ -43,30 +40,10 @@ func main() {
 func loadCA() (cert tls.Certificate, err error) {
 	// TODO(kr): check file permissions
 	cert, err = tls.LoadX509KeyPair(certFile, keyFile)
-	if os.IsNotExist(err) {
-		cert, err = genCA()
-	}
 	if err == nil {
 		cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
 	}
 	return
-}
-
-func genCA() (cert tls.Certificate, err error) {
-	err = os.MkdirAll(dir, 0700)
-	if err != nil {
-		return
-	}
-	certPEM, keyPEM, err := mitm.GenCA(hostname)
-	if err != nil {
-		return
-	}
-	cert, _ = tls.X509KeyPair(certPEM, keyPEM)
-	err = ioutil.WriteFile(certFile, certPEM, 0400)
-	if err == nil {
-		err = ioutil.WriteFile(keyFile, keyPEM, 0400)
-	}
-	return cert, err
 }
 
 type cloudToButtResponse struct {
